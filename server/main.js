39eb5dcb1444ccb72950bbdd38b58678a3be0291
Amy Bakameyer <bymak87@gmail.com>
2017-01-30 23:33:01 -0600

    added a collection

--- server/main.js
+++ server/main.js
@@ -1,8 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
+if(Meteor.isServer){
+    Meteor.startup(() => {
   // code to run on server at startup
   console.log("code is running server");
 
+  var Images = new Mongo.Collection("images");
 
-});
+  if (Images.find().count() === 0){
+    Images.insert(
+      {
+        img_src: "https://s-media-cache-ak0.pinimg.com/736x/b9/3b/dc/b93bdc427c9aee78e91e340b182e4777.jpg",
+        img_alt: "persian cats"
+      },
+      {
+        img_src: "http://animalia-life.club/data_images/munchkin-cat/munchkin-cat5.jpg",
+        img_alt: "munchkin cats"
+      }
+
+    )
+    }// end of if
+      });
+  } // end of if server
